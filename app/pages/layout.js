287214062918e0a2ee3fb528bf648a602dbe8505
Samson Fu <samson_fu@hotmail.com>
2023-12-15 15:54:48 +0800

    testing pages

---
+++ app/pages/layout.js
@@ -0,0 +1,11 @@
+
+export default function RootLayout({ children }) {
+    return (
+      <>
+        <div>pages header</div>
+        {children}
+        <div>pages footer</div>
+      </>
+    )
+}
+  
